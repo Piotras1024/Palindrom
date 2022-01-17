b56552179370286cae755bb70a404614813444d2
Piotr <piotr.w.dabrowski2@gmail.com>
2022-01-17 13:17:01 +0100

    Palindrom - wersja docelowa

--- palindrom.py
+++ palindrom.py
@@ -1,12 +1,18 @@
+import string
+
 def palindrom_check(napis):
     napis_lower = napis.lower()
+    words = napis_lower.split()
+    table = str.maketrans("", "", string.punctuation)
+    stripped = [w.translate(table) for w in words]
+    assembled = " ".join(stripped)
     index_start = 0
-    index_end = int(len(napis_lower)/2 )
+    index_end = int(len(assembled)/2 )
     for index in range(index_start, index_end):
-        if napis_lower[index] != napis_lower[len(napis) -1 -index]:
+        if assembled[index] != assembled[len(assembled) -1 -index]:
             return False
     return True
 
 
-for napis in ('aabbaa', 'kAjaK', 'abba abba abba', 'niedziala'):
+for napis in ('a,,abba!!!!a', 'kAjaK', 'abba!!!,.,., abba abba', 'niedziala', "aa   !!!,,...!!??$$  aa"):
     print(f" {napis} : {palindrom_check(napis)}")
