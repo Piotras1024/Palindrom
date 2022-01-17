cda8a49b5c0c5266d77941a275c42d514b61e90f
Piotr <piotr.w.dabrowski2@gmail.com>
2022-01-17 13:52:59 +0100

    Palindrom Wersja declowa 2

--- palindrom.py
+++ palindrom.py
@@ -1,12 +1,23 @@
+def remove_ignored(napis):
+    new_chars = []
+    for char in napis:
+        ascii_nr = ord(char)
+        # print(f"char: {char} ascii: {ascii_nr}")
+        if 97 <= ascii_nr <= 122 or 48 <= ascii_nr <= 57:
+            new_chars.append(char)
+    return "".join(new_chars)
+
 def palindrom_check(napis):
     napis_lower = napis.lower()
+    nowy_napis = remove_ignored(napis_lower)
+    print(f'nowy_napis: {nowy_napis}')
     index_start = 0
-    index_end = int(len(napis_lower)/2 )
+    index_end = int(len(nowy_napis)/2 )
     for index in range(index_start, index_end):
-        if napis_lower[index] != napis_lower[len(napis) -1 -index]:
+        if nowy_napis[index] != nowy_napis[len(nowy_napis) -1 -index]:
             return False
     return True
 
 
-for napis in ('aabbaa', 'kAjaK', 'abba abba abba', 'niedziala'):
+for napis in ('aabbaa', 'kAja;;;;K', 'abba abba abba', 'niedziala'):
     print(f" {napis} : {palindrom_check(napis)}")
