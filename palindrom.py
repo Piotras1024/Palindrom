26ee2a9f1a14f0eedee77070b0fc8f2af29e0ae8
Piotr <piotr.w.dabrowski2@gmail.com>
2022-01-17 12:45:02 +0100

    Niewrazliwy na wielkosc znakow

--- palindrom.py
+++ palindrom.py
@@ -1,12 +1,12 @@
 def palindrom_check(napis):
-    indexstart = 0
-    indexend = int(len(napis)/2 )
-    for index in range(indexstart, indexend):
-        if napis[index] != napis[len(napis) -1 -index]:
+    napis_lower = napis.lower()
+    index_start = 0
+    index_end = int(len(napis_lower)/2 )
+    for index in range(index_start, index_end):
+        if napis_lower[index] != napis_lower[len(napis) -1 -index]:
             return False
     return True
 
 
-
-for napis in ('aabbaa', 'kajak', 'abba abba abba', 'niedziala'):
+for napis in ('aabbaa', 'kAjaK', 'abba abba abba', 'niedziala'):
     print(f" {napis} : {palindrom_check(napis)}")
